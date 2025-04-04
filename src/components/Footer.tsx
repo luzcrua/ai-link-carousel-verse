e4c69e2da9dacb855d9551d9bb4356ffd414b961
luzcrua <tentandodarcertonoyt@gmail.com>
2025-04-04 00:19:10 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,7 +12,7 @@ const Footer: React.FC = () => {
             Tem alguma pergunta ou proposta? Preencha o formulário abaixo para entrar em contato.
           </p>
           
-          <div className="w-full h-[600px] overflow-hidden rounded-lg">
+          <div className="w-full h-[800px] overflow-hidden rounded-lg">
             <iframe 
               src="https://arinelsonform.netlify.app/" 
               title="Formulário de Contato"
